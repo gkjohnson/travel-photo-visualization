747908ca411c408c95dc55eb907c61f9d261544e
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-08-06 13:10:08 -0700

    Add ability to sideload in custom locations for images

--- index.js
+++ index.js
@@ -9,12 +9,51 @@ const dest = args.dest
 const minDate = args.min ? new Date(args.min) : null
 const maxDate = args.max ? new Date(args.max) : null
 
+// load in the custom locations
+const customLocations = {}
+if(args['custom-locations']) {
+    const customLocationsFile = args['custom-locations']
+    try {
+        let file = fs.readFileSync(customLocationsFile, 'utf-8')
+        
+        // remove comments
+        file = file.replace(/#[^\n]\n/g, '')
+
+        // read in each line
+        const lines = file.split(/\n/g)
+        lines.forEach(line => {
+            const split = line.trim().split(/\s+/g)
+            
+            // process into "imagename : { lat, long }"
+            const image = split[0]
+            const lat = parseFloat(split[1])
+            const lon = parseFloat(split[2])
+
+            let err = split.length !== 3 || Number.isNaN(lat) || Number.isNaN(lon)
+
+            if (err) {
+                console.error(`Error prcessing custom-lcations line "${line}"`)
+            } else {
+                customLocations[image] = { lat, lon }
+            }
+        })
+    } catch(e) {
+        console.error(`Error reading custom-locations file "${customLocationsFile}"`)
+        console.error(e)
+    }
+}
+
+
 // helper for processing the exif date to a Date()
 const exifDate2Date = str => {
     const split = str.split(' ')
     split[0] = split[0].replace(/:/g, '/')
     return new Date(split.join(' '))
 }
+
+// convert dms coordinates into decimal coordinates
+// https://en.wikipedia.org/wiki/Decimal_degrees
+const dms2dec = (d, m, s, ref) => (ref === 'S' || ref === 'W' ? -1 : 1) * (d + m / 60 + s / 3600)
 
 // synchronous processing of the exif data
 const getExif = deasync((path, done) => new ExifImage({ image: path }, done))
@@ -35,7 +74,7 @@ const gpsdata = fs
         return res
     })
     // filter to data that provides the gps data
-    .filter(exif => !!(exif.gps.GPSLatitude || exif.gps.GPSLongitude))
+    .filter(exif => !!(exif.gps.GPSLatitude || exif.gps.GPSLongitude) || exif.__filename in customLocations)
 
     // filter to the time range
     .filter(exif => (!minDate || exif.date > minDate) && (!maxDate || exif.date < maxDate))
@@ -45,8 +84,16 @@ const gpsdata = fs
         const out = {}
         out.filename = exif.__filename
         out.date = exif.__jsDate
-        out.lat = [...exif.gps.GPSLatitude, exif.gps.GPSLatitudeRef]
-        out.lon = [...exif.gps.GPSLongitude, exif.gps.GPSLongitudeRef]
+        
+        if (exif.__filename in customLocations) {
+            out.lat = customLocations[exif.__filename].lat
+            out.lon = customLocations[exif.__filename].lon
+            out.custom = true        
+        } else {
+            out.lat = dms2dec(...exif.gps.GPSLatitude, exif.gps.GPSLatitudeRef)
+            out.lon = dms2dec(...exif.gps.GPSLongitude, exif.gps.GPSLongitudeRef)
+            out.custom = false
+        }
 
         return out
     })
